0781d4bb843658bd31ed0b6c556ecad66d01f5e2
vonmutinda <maxwellwambua@gmail.com>
2018-05-01 18:32:26 +0300

    functional full page (overlay menu)incorporated!

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-$(document).ready(function(){
-	
 
-});
+/**TRANSITION HORIZONTALLY**/
+
+function openNav(){
+	document.getElementById('nav').style.width = "100%";
+}
+
+function closeNav(){
+	document.getElementById('nav').style.width = "0%";
+}
+
+
+
+
+/*TRANSITION VERTICALLY*/
+
+function openNav(){
+	document.getElementById('nav').style.height = "100%";
+}
+
+function closeNav(){
+	document.getElementById('nav').style.height = "0";
+}
